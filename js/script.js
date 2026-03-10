d76f5c0b32a8bef744f343aaaaacc33e285a4d64
JohanAnd10s <62313155+JohanAnd10s@users.noreply.github.com>
2026-03-09 21:26:29 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -83,8 +83,10 @@ document.addEventListener("DOMContentLoaded", function(){
 const menuBtn = document.getElementById("menu-btn");
 const nav = document.getElementById("nav");
 
-menuBtn.addEventListener("click", function(){
-    nav.classList.toggle("active");
-});
+if(menuBtn && nav){
+    menuBtn.addEventListener("click", function(){
+        nav.classList.toggle("active");
+    });
+}
 
 });
