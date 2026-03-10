1bf2df3e5917a57b145d437fd622bc99d3eb5fce
JohanAnd10s <62313155+JohanAnd10s@users.noreply.github.com>
2026-03-09 23:31:31 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -271,7 +271,17 @@ transition:.3s;
 
 }
 
+// MENU HAMBURGUESA
+document.addEventListener("DOMContentLoaded", function(){
 
+const menuBtn = document.getElementById("menu-btn");
+const nav = document.getElementById("nav");
+
+menuBtn.addEventListener("click", function(){
+nav.classList.toggle("active");
+});
+
+});
 /* NAV */
 
 nav{
